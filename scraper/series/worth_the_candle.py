31fd330150cadbf6157169a9065e41fe624132db
omstrumpf <omstrumpf@gmail.com>
2019-06-23 17:42:38 -0400

    getting rid of async requests

--- scraper/series/worth_the_candle.py
+++ scraper/series/worth_the_candle.py
@@ -1,7 +1,6 @@
-import asyncio
 from datetime import datetime
-import aiohttp
 import bs4
+import requests
 
 from scraper.chapter import Chapter
 from .series import Series
@@ -19,62 +18,43 @@ class WorthTheCandle(Series):
         return "cthuluraejepsen"
 
     @staticmethod
-    async def __scrape_chapter_body(session, url):
-        async with session.get(f"{BASE_URL}{url}") as resp:
-            body = await resp.text()
-            soup = bs4.BeautifulSoup(body, features="lxml")
+    def __scrape_chapter_body(url):
+        response = requests.get(f"{BASE_URL}{url}")
+        soup = bs4.BeautifulSoup(response.content, features="lxml")
 
-            return str(soup.find("div", "chapter"))
+        return str(soup.find("div", "chapter"))
 
-    async def __scrape_new(self):
-        async with aiohttp.ClientSession() as session:
-            async with session.get(
-                f"{BASE_URL}/works/11478249/navigate", timeout=60
-            ) as resp:
-                soup = bs4.BeautifulSoup(await resp.text(), features="lxml")
+    def __scrape_new(self):
+        response = requests.get(f"{BASE_URL}/works/11478249/navigate")
+        soup = bs4.BeautifulSoup(response.content, features="lxml")
 
-                entries = soup.find("ol", class_="chapter index group").find_all("li")
+        entries = soup.find("ol", class_="chapter index group").find_all("li")
 
-                empty_chapters = []
+        chapters = []
 
-                for entry in entries:
-                    link = entry.find("a")
-                    date = entry.find("span")
-                    index = int(link.text[: link.text.find(".")])
+        for entry in entries:
+            link = entry.find("a")
+            date = entry.find("span")
+            index = int(link.text[: link.text.find(".")])
 
-                    if index > self.state.get("max_index", 0):
-                        empty_chapters.append(
-                            (
-                                Chapter(
-                                    WorthTheCandle.title(),
-                                    WorthTheCandle.author(),
-                                    link.text,
-                                    datetime.timestamp(
-                                        datetime.strptime(date.text, "(%Y-%m-%d)")
-                                    ),
-                                    None,
-                                    index,
-                                ),
-                                link.attrs["href"],
-                            )
-                        )
-
-            chapter_bodies = asyncio.gather(
-                *(
-                    WorthTheCandle.__scrape_chapter_body(session, url)
-                    for (chapter, url) in empty_chapters
+            if index > self.state.get("max_index", 0):
+                chapters.append(
+                    Chapter(
+                        WorthTheCandle.title(),
+                        WorthTheCandle.author(),
+                        link.text,
+                        datetime.timestamp(
+                            datetime.strptime(date.text, "(%Y-%m-%d)")
+                        ),
+                        WorthTheCandle.__scrape_chapter_body(link.attrs["href"]),
+                        index,
+                    )
                 )
-            )
-
-            chapters = [chapter for (chapter, url) in empty_chapters]
-
-            for i, body in enumerate(await chapter_bodies):
-                chapters[i].body = body
 
         return sorted(chapters)
 
     def scrape(self) -> [Chapter]:
-        chapters = asyncio.run(self.__scrape_new())
+        chapters = self.__scrape_new()
 
         if chapters:
             self.state["max_index"] = max(
