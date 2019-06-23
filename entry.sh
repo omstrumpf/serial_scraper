50f4be431aa1a85ed3fd8decd3b915bcbfe3cd82
omstrumpf <omstrumpf@gmail.com>
2019-06-23 17:58:08 -0400

    only dry run if state.json doesn't exist

--- entry.sh
+++ entry.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-echo "Doing a dry run to populate state.json"
-python3 -m scraper.main --dry-run
+if [ -f "state.json" ]; then
+    echo "Doing a dry run to populate state.json"
+    python3 -m scraper.main --dry-run
+fi
 
 echo "Registering cron job"
 crond -f
